593a1fa3b4c6bd229e57a9416b6498abe365f0f9
sirlucidae <lucidae@outlook.com.au>
2022-01-01 21:53:41 +1000

    initial commit

---
+++ script.py
@@ -0,0 +1,6 @@
+#print("This line will be printed.")
+#x = 1
+#if x == 1:
+    #indented 4 spaces
+#    print("x is 1.")
+print("Hello, World!")
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+NAME="World"
+echo "Hello, $NAME!"
